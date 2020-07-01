f68e027a184309fc45e8967bcf94202ad12df1cf
Oksana <Oksana@MacBook-Pro-LFam.local>
2020-07-01 22:25:04 +0300

    header

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,35 @@ function requireAll(r) {
   }
   
 requireAll(require.context('../icons/', true, /\.svg$/));
+
+$( document ).ready(function() {
+
+  // add eventListeners
+
+  $('.js-menu-button').on('click', openMobileMenu);
+  $('.js-nav').on('click', scrollToBlock);
+
+  // mobile menu 
+  function openMobileMenu() {
+    $('.js-menu-button').toggleClass('menu-button--opened');
+    $('.js-nav').slideToggle();
+    $('.js-menu-overlay').toggleClass('menu-overlay--active');
+  }
+  
+  function scrollToBlock(e) {
+    if($(e.target).hasClass('js-nav_link')) {
+      let anchor = `#${$(e.target).data('id')}`;
+
+      if ($(anchor).length < 1) return
+
+      $('html, body').animate({
+          scrollTop: $(anchor).offset().top
+      }, 500);
+
+      if ($('.js-menu-overlay').hasClass('menu-overlay--active')) {
+        $('.js-menu-button').click();
+      }
+    }
+  }
+
+});
